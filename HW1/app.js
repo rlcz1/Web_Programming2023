178e906452edcbebbc652bf58f649d3402937952
rlcz1 <rbf0311@gmail.com>
2023-10-31 23:57:31 +0900

    HW1

--- HW1/app.js
+++ HW1/app.js
@@ -17,6 +17,30 @@ class App {
         document.getElementById("modifyModalEdit").addEventListener('click', this.modifyModalEdit);
         document.getElementById("modifyModalDelete").addEventListener('click', this.modifyModalDelete);
 
+        document.getElementById("todoCheckbox").addEventListener('change', this.checkBoxFilter);
+        document.getElementById("meetingCheckbox").addEventListener('change', this.checkBoxFilter);
+        document.getElementById("ideaCheckbox").addEventListener('change', this.checkBoxFilter);
+        document.getElementById("shoppingCheckbox").addEventListener('change', this.checkBoxFilter);
+    }
+
+    checkBoxFilter = () => {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        const selectedFilters = [];
+
+        checkboxes.forEach(checkbox => {
+            if (checkbox.checked) {
+                selectedFilters.push(checkbox.getAttribute('data-filter'));
+            }
+        });
+
+        const todoItems = document.querySelectorAll('.todo_item');
+        todoItems.forEach(todoItem => {
+            if (selectedFilters.includes(todoItem.getAttribute('data-type'))) {
+                todoItem.style.display = "block";
+            } else {
+                todoItem.style.display = "none";
+            }
+        });
     }
 
     makeJournal() {
@@ -77,34 +101,45 @@ class App {
 
         modal_cidx.value = cidx;
         modal_idx.value = idx;
+
+        modal.querySelector('input[name="title"]').value = "";
+        modal.querySelector('textarea[name="content"]').value = "";
+        modal.querySelector('select[name="type"]').value = "todo";
     }
 
     modifyModal = (e) => {
         e.stopPropagation();
 
         const target = e.target;
+
         const cidx = target.getAttribute('c-idx');
         const idx = target.getAttribute('idx');
+        const type = target.getAttribute('data-type');
+        const content = target.getAttribute('data-content');
+        const title = target.getAttribute('data-title');
 
         const modal = document.getElementById('modifyModal');
         modal.style.display = "block";
 
         const modal_cidx = modal.querySelector('input[name="cidx"]');
         const modal_idx = modal.querySelector('input[name="idx"]');
+        const modal_type = modal.querySelector('input[name="hidden_type"]');
+        const modal_content = modal.querySelector('input[name="hidden_content"]');
+        const modal_title = modal.querySelector('input[name="hidden_title"]');
 
         modal_cidx.value = cidx;
         modal_idx.value = idx;
+        modal_type.value = type;
+        modal_content.value = content;
+        modal_title.value = title;
 
-        const title = modal.querySelector('input[name="title"]');
-        const content = modal.querySelector('textarea[name="content"]');
-        const type = modal.querySelector('select[name="type"]');
+        const title_input = modal.querySelector('input[name="title"]');
+        const content_input = modal.querySelector('textarea[name="content"]');
+        const type_input = modal.querySelector('select[name="type"]');
 
-        const tbody = document.querySelector('tbody');
-        const td = tbody.querySelector(`td[c-idx="${cidx}"][idx="${idx}"]`);
-        const div = td.querySelector('div');
-        title.value = div.textContent;
-        content.value = div.getAttribute('data-content');
-        type.value = div.getAttribute('data-type');
+        title_input.value = title;
+        content_input.value = content;
+        type_input.value = type;
 
         document.querySelector('#modifyModal input[name="title"]').setAttribute('disabled', true);
         document.querySelector('#modifyModal textarea').setAttribute('disabled', true);
@@ -128,6 +163,7 @@ class App {
         
         // calendar 영역의 todo
         const tbody = document.querySelector('tbody');
+        
         const td = tbody.querySelector(`td[c-idx="${cidx}"][idx="${idx}"]`);
         const txt = document.createTextNode(title);
         const div = document.createElement('div');
@@ -135,6 +171,7 @@ class App {
         div.setAttribute('class', 'todo_item '+type);
         div.setAttribute('data-content', content);
         div.setAttribute('data-type', type);
+        div.setAttribute('data-title', title);
         div.setAttribute('c-idx', cidx);
         div.setAttribute('idx', idx);
         td.appendChild(div);
@@ -148,6 +185,7 @@ class App {
         list_div.prepend(list_input);
         list.appendChild(list_div);
 
+        list_div.addEventListener('click', this.deleteListItem);
         div.addEventListener('click', this.modifyModal);
 
         alert("계획이 추가되었습니다.");
@@ -168,9 +206,12 @@ class App {
 
         const idx = modal.querySelector('input[name="idx"]').value;
         const cidx = modal.querySelector('input[name="cidx"]').value;
+        const type = modal.querySelector('input[name="hidden_type"]').value;
+        const content = modal.querySelector('input[name="hidden_content"]').value;
+        const title = modal.querySelector('input[name="hidden_title"]').value;
 
-        const target = document.querySelector(`td div[c-idx="${cidx}"][idx="${idx}"]`);
-        const list_target = document.querySelector(`#list div[c-idx="${cidx}"][idx="${idx}"]`);
+        const target = document.querySelector('tbody').querySelector(`.todo_item[c-idx="${cidx}"][idx="${idx}"][data-type="${type}"][data-content="${content}"][data-title="${title}"]`);
+        const list_target = document.querySelector('#list').querySelector(`.todo_item[c-idx="${cidx}"][idx="${idx}"][data-type="${type}"][data-content="${content}"][data-title="${title}"]`);
 
         target.remove();
         list_target.remove();
@@ -182,25 +223,31 @@ class App {
     saveModifyModal = (e) => {
         const modal = e.target.parentNode.parentNode;
 
-        const title = modal.querySelector('input[name="title"]').value;
-        const content = modal.querySelector('textarea[name="content"]').value;
-        const type = modal.querySelector('select[name="type"]').value;
+        const title_val = modal.querySelector('input[name="title"]').value;
+        const content_val = modal.querySelector('textarea[name="content"]').value;
+        const type_val = modal.querySelector('select[name="type"]').value;
 
         const idx = modal.querySelector('input[name="idx"]').value;
         const cidx = modal.querySelector('input[name="cidx"]').value;
+        const type = modal.querySelector('input[name="hidden_type"]').value;
+        const content = modal.querySelector('input[name="hidden_content"]').value;
+        const title = modal.querySelector('input[name="hidden_title"]').value;
 
-        const target = document.querySelector(`td div[c-idx="${cidx}"][idx="${idx}"]`);
-        const list_target = document.querySelector(`#list div[c-idx="${cidx}"][idx="${idx}"]`);
-        target.textContent = title;
-        target.setAttribute('data-content', content);
-        target.setAttribute('data-type', type);
-        target.setAttribute('class', 'todo_item '+type);
+        const target = document.querySelector('tbody').querySelector(`.todo_item[c-idx="${cidx}"][idx="${idx}"][data-type="${type}"][data-content="${content}"][data-title="${title}"]`);
+        const list_target = document.querySelector('#list').querySelector(`.todo_item[c-idx="${cidx}"][idx="${idx}"][data-type="${type}"][data-content="${content}"][data-title="${title}"]`);
+
+        target.textContent = title_val;
+        target.setAttribute('data-content', content_val);
+        target.setAttribute('data-type', type_val);
+        target.setAttribute('data-title', title_val);
+        target.setAttribute('class', 'todo_item '+type_val);
 
         const list_input = document.createElement('input');
-        list_target.textContent = title;
-        list_target.setAttribute('data-content', content);
-        list_target.setAttribute('data-type', type);
-        list_target.setAttribute('class', 'todo_item '+type);
+        list_target.textContent = title_val;
+        list_target.setAttribute('data-content', content_val);
+        list_target.setAttribute('data-type', type_val);
+        list_target.setAttribute('data-title', title_val);
+        list_target.setAttribute('class', 'todo_item '+type_val);
         list_input.setAttribute('type', 'radio');
         list_input.setAttribute('name', 'todo');
         list_target.prepend(list_input);
@@ -212,6 +259,25 @@ class App {
     modalDisable = () => {
         document.getElementById("modal").style.display = "none";
         document.getElementById("modifyModal").style.display = "none";
+    }
+
+    deleteListItem = (e) => {
+        let target = e.target;
+
+        if (target.tagName == "INPUT") {
+            target = target.parentNode;
+        }
+
+        const cidx = target.getAttribute('c-idx');
+        const idx = target.getAttribute('idx');
+        const type = target.getAttribute('data-type');
+        const content = target.getAttribute('data-content');
+        const title = target.getAttribute('data-title');
+
+        alert("삭제되었습니다.")
+
+        document.querySelector('#list').querySelector(`.todo_item[c-idx="${cidx}"][idx="${idx}"][data-type="${type}"][data-content="${content}"][data-title="${title}"]`).remove();
+        document.querySelector('tbody').querySelector(`.todo_item[c-idx="${cidx}"][idx="${idx}"][data-type="${type}"][data-content="${content}"][data-title="${title}"]`).remove();
     }
 }
 
